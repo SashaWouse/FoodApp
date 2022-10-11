970aaff18e6303561f7997cbbfe5219784fb41f8
SashaWouse <snowpill@hotmail.fi>
2022-10-11 11:52:06 +0300

    Searching by meal,search by typing

--- app/src/main/java/com/example/foodapp/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/foodapp/fragments/SearchFragment.kt
@@ -5,12 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
+import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.foodapp.R
 import com.example.foodapp.activities.MainActivity
 import com.example.foodapp.activities.MealActivity
 import com.example.foodapp.adapters.MealsAdapter
 import com.example.foodapp.databinding.FragmentSearchBinding
 import com.example.foodapp.viewModel.HomeViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SearchFragment : Fragment() {
 
@@ -31,6 +39,51 @@ class SearchFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentSearchBinding.inflate(inflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        prepareRecyclerView()
+
+        binding.imgSearchArrow.setOnClickListener {
+            searchMeals()
+        }
+        observeSearchedMealsLiveData()
+
+        // coroutine for automate searching
+        var searchJob:Job? = null
+        binding.searchBox.addTextChangedListener { searchQuery ->
+            searchJob?.cancel()
+            searchJob = lifecycleScope.launch {
+                delay(500) // for calling api every 500msec
+                viewModel.searchMeals(searchQuery.toString())
+            }
+        }
+
+    }
+
+    private fun observeSearchedMealsLiveData() {
+        viewModel.observeSearchMealsLiveData().observe(viewLifecycleOwner, Observer { mealsList ->
+
+            searchRecyclerviewAdapter.differ.submitList(mealsList)
+
+        })
+    }
+
+    private fun searchMeals() {
+        val searchQuery = binding.searchBox.text.toString()
+        if(searchQuery.isNotEmpty()){
+            viewModel.searchMeals(searchQuery)
+        }
+    }
+
+    private fun prepareRecyclerView() {
+        searchRecyclerviewAdapter = MealsAdapter()
+        binding.rvSearchedMeal.apply {
+            layoutManager = GridLayoutManager(context, 2,GridLayoutManager.VERTICAL,false)
+            adapter = searchRecyclerviewAdapter
+        }
     }
 
 }
